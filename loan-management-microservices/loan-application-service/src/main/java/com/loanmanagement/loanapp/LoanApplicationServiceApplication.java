caa6e510d68be9c68a13d1c332b15cf178febed2
riddhimaBhanja <richabhanja2003@gmail.com>
2026-01-07 02:37:33 +0530

    modified the loan-approval-service

--- loan-management-microservices/loan-application-service/src/main/java/com/loanmanagement/loanapp/LoanApplicationServiceApplication.java
+++ loan-management-microservices/loan-application-service/src/main/java/com/loanmanagement/loanapp/LoanApplicationServiceApplication.java
@@ -3,14 +3,12 @@ package com.loanmanagement.loanapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * Main application class for Loan Application Service
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableJpaAuditing
 public class LoanApplicationServiceApplication {
 
     public static void main(String[] args) {
