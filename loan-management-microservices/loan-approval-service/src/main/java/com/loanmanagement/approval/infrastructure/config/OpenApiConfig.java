0ab054e441afeeef70c7c58a776db254368315ba
riddhimaBhanja <richabhanja2003@gmail.com>
2026-01-07 22:55:33 +0530

    modified loan-approval-service

---
+++ loan-management-microservices/loan-approval-service/src/main/java/com/loanmanagement/approval/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,56 @@
+package com.loanmanagement.approval.infrastructure.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+/**
+ * OpenAPI/Swagger Configuration for Loan Approval Service
+ */
+@Configuration
+public class OpenApiConfig {
+
+    @Value("${server.port:8083}")
+    private String serverPort;
+
+    @Bean
+    public OpenAPI loanApprovalServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Loan Approval Service API")
+                        .description("Microservice for loan approval workflows, credit checks, and risk assessment")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Loan Management Team")
+                                .email("support@loanmanagement.com"))
+                        .license(new License()
+                                .name("Proprietary")
+                                .url("https://loanmanagement.com/license")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:" + serverPort)
+                                .description("Local Development Server"),
+                        new Server()
+                                .url("http://localhost:8080/loan-approval")
+                                .description("API Gateway")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .name("Bearer Authentication")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter JWT token")));
+    }
+}
